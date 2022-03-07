1c964758411f46a93839d5c32eb54e84e135edbf
Sadullah TANRIKULU <sadullahtanrikulu@gmail.com>
2022-03-07 13:39:30 +0300

    assignment_fibonacci.py was created

---
+++ assignment_fibonacci.py
@@ -0,0 +1,25 @@
+"""
+Task : Create a list consisting of Fibonacci numbers from 1 to 55 using control flow statements and range() function.
+The desired output is like this:
+fibonacci →  [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+"""
+
+fibonacci_list = [1, 1]
+for i in range(8):
+    number = fibonacci_list[i] + fibonacci_list[i + 1]
+    fibonacci_list.append(number)
+    # print(number, fibonacci_list)
+
+print("fibonacci list: ", fibonacci_list)
+
+
+# second way
+# a=1
+# b=1
+# fibonacci=[a,b]
+
+# for i in  range(10):
+#     a, b = b, a + b
+#     print("a:", a, "b:", b)
+#     fibonacci.append(b)
+# print(fibonacci)
